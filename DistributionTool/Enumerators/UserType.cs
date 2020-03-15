614315c863363d8e4584f9aa6f70bc4dde793df8
Marcin Kupiński <Mlorism@gmail.com>
2020-03-15 22:45:40 +0100

    Creation of User model, UserTypes and start of EF configuration.

---
+++ DistributionTool/Enumerators/UserType.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistributionTool.Enumerators
+{
+	public enum UserType
+	{
+		/// <summary>
+		/// Admin can create, delete, update or suspend user account.
+		/// </summary>
+		Admin,
+		/// <summary>
+		/// AllocationManager can change allocator permissions.
+		/// </summary>
+		AllocationManager,
+		/// <summary>
+		/// Allocator is responsible for creating distributions of stock to stores.
+		/// </summary>
+		Allocator
+	}
+}
---
+++ DistributionTool/Models/User.cs
@@ -0,0 +1,40 @@
+﻿using DistributionTool.Enumerators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace DistributionTool.Models
+{
+	/// <summary>
+	/// Class model representing application user.
+	/// </summary>
+	class User
+	{	/// <summary>
+		/// Identification numer of a user.
+		/// </summary>
+		public int Id { get; set; }
+		/// <summary>
+		/// Name of the user. It is recommended to use the first and last name as the username, and if the number of users is large, use also the middle name.
+		/// </summary>
+		public string Name { get; set; }
+		/// <summary>
+		/// Hashed password with salt.
+		/// </summary>
+		public byte[] Password { get; set; }
+		/// <summary>
+		/// Generated password salt key for additional protection.
+		/// </summary>
+		public byte[] PasswordSalt { get; set; }
+		/// <summary>
+		/// User type is associated with access to specific parts of the application.
+		/// </summary>
+		public UserType Type { get; set; }
+		/// <summary>
+		/// User account could be active or blocked.
+		/// </summary>
+		public bool AccountActive { get; set; }
+	}
+}
