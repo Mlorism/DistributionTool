72f63e9851496e938b1d90634c2db095477e6594
Marcin Kupiński <Mlorism@gmail.com>
2020-03-16 20:50:59 +0100

    ApplicationDbContext created

---
+++ DistributionTool/Migrations/202003161856317_InitialModel.cs
@@ -0,0 +1,30 @@
+﻿namespace DistributionTool.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialModel : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false),
+                        Password = c.Binary(),
+                        PasswordSalt = c.Binary(),
+                        Type = c.Int(nullable: false),
+                        AccountActive = c.Boolean(nullable: false, defaultValue: true),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Users");
+        }
+    }
+}
---
+++ DistributionTool/Models/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace DistributionTool.Models
+{
+	class ApplicationDbContext : DbContext
+	{
+		public DbSet<User> Users { get; set; }
+
+		public ApplicationDbContext()
+			: base("name=DefaultConnection")
+		{
+
+		}
+	}
+}
