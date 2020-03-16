53549edaef6f8b2c9f413dac47672b02a4d761a9
Marcin Kupiński <Mlorism@gmail.com>
2020-03-16 23:28:43 +0100

    BaseViewModel and Login MVVM elements created.

---
+++ DistributionTool/ViewModels/BaseViewModel.cs
@@ -0,0 +1,35 @@
+﻿using DistributionTool.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistributionTool.ViewModels
+{
+	class BaseViewModel : INotifyPropertyChanged, ITab
+	{
+		public string TabName { get; set; }
+
+		#region INotifyPropertyChanged
+		/// <summary>
+		/// INotifyPropertyChanged implementation
+		/// </summary>
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected void OnPropertyChange (string propertyName)
+		{
+			try
+			{
+				PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+			}
+
+			catch (Exception)
+			{
+				throw new Exception("Invalid property name: " + propertyName);
+			}
+		} //OnPropertyChange()
+		#endregion
+	}
+}
---
+++ DistributionTool/ViewModels/LoginViewModel.cs
@@ -0,0 +1,67 @@
+﻿using DistributionTool.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DistributionTool.ViewModels
+{
+	class LoginViewModel : BaseViewModel
+	{
+		#region Commands	
+
+		#endregion
+
+		#region Properties	
+		public string UserName { get; set; }
+		public string Password { get; set; }
+		private int logingAttempts = 0;
+		#endregion
+
+		#region Connection to the database		
+		private static ApplicationDbContext dbContext = new ApplicationDbContext();
+		private static ApplicationDbContext context
+		{
+			get 
+			{
+				try
+				{
+					return dbContext;
+				}
+
+				catch (Exception)
+				{
+					return null;
+					throw new Exception("Database connection error.");					
+				}			
+			}
+
+			set => dbContext = value;
+		}
+		#endregion
+
+		#region Constructor
+		public LoginViewModel()
+		{
+			TabName = "LoginPage";
+		}
+		#endregion
+
+		#region Methods
+		public static bool LogInAction(string name, string password)
+		{
+			if (context.Users.FirstOrDefault(u => u.Name == name) is User user)
+			{
+				return true;
+			} // User name exist in database
+
+			else
+			{
+				return false;
+			} // There is no user with that name
+		}
+		#endregion
+	}
+}
