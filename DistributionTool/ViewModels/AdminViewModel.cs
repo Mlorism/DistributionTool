2662e567aaed73b1a2b0297d29878ae175bab463
Marcin Kupiński <Mlorism@gmail.com>
2020-03-22 20:10:22 +0100

    User filter in Admin page implemented.

--- DistributionTool/ViewModels/AdminViewModel.cs
+++ DistributionTool/ViewModels/AdminViewModel.cs
@@ -1,25 +1,60 @@
 ﻿using DistributionTool.Interfaces;
+using DistributionTool.Models;
 using DistributionTool.ViewModels.Lists;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
 
 namespace DistributionTool.ViewModels
 {
 	class AdminViewModel : BaseViewModel, ITab
 	{
 		#region Properties
-		private readonly ICollectionView usersFilter;
+		/// <summary>
+		/// Filter on UserList
+		/// </summary>		
 
+		public User defaultUser = new User();
+		private string findUserText;
+
+		public string FindUserText
+		{
+			get { return findUserText; }
+			set 
+			{ 
+				findUserText = value;
+				FilterList();				
+			}
+		}
+
+
+		public ICollectionView userFilteredList { get; set; }
 		#endregion
 
 		#region Constructor
 		public AdminViewModel()
 		{
-			TabName = "Admin panel";			
+			TabName = "Admin panel";
+
+			var usersSourceList = new CollectionViewSource() { Source = UsersListViewModel.Instance.UsersList };
+			userFilteredList = usersSourceList.View;
+
+		}
+		#endregion
+
+		#region Methods
+		private void FilterList()
+		{
+			Predicate<object> Filter = new Predicate<object>(item => ((User)item).Name.ToLower().Contains(FindUserText.ToLower()));
+			// Predicate<object> Filter = new Predicate<object>(item => ((User)item).Name.ToLower().Contains(FindUserText.ToLower()));
+			userFilteredList.Filter = Filter;
+			OnPropertyChange("userFilteredList");
 		}
 		#endregion
 
