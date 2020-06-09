a46828f2593431ada0358f9538203634517fa628
Marcin Kupiński <Mlorism@gmail.com>
2020-06-09 21:59:23 +0200

    Distribution methods created.

--- DistributionTool/Enumerators/DistributionMethods.cs
+++ DistributionTool/Enumerators/DistributionMethods.cs
@@ -8,6 +8,26 @@ namespace DistributionTool.Enumerators
 {
 	public enum DistributionMethods
 	{
-		
+		/// <summary>
+		/// Keep only the minimum. If the number falls below the minimum, distribute the package.
+		/// </summary>
+		KeepMinimum,
+		/// <summary>
+		/// Keep at least set cover based on past sales.  
+		/// </summary>
+		WeeksOfSales,
+		/// <summary>
+		/// Calculate future sales based on the sales curve for the subgroup.
+		/// </summary>
+		SubgroupTrend,
+		/// <summary>
+		/// Calculate future sales based on the sales curve for the group.
+		/// </summary>
+		GroupTrend,
+		/// <summary>
+		/// Distribute all remaining stock based on current store stock and grade.
+		/// </summary>
+		FinalDistribution,
+
 	}
 }
