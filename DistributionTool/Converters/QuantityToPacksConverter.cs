fa7b3bee6e15dbb0b0604b38d8a4e708493ff9d7
Marcin Kupiński <Mlorism@gmail.com>
2020-06-13 00:08:20 +0200

    QuantityToPacksConverter created.

---
+++ DistributionTool/Converters/QuantityToPacksConverter.cs
@@ -0,0 +1,33 @@
+﻿using DistributionTool.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace DistributionTool.Converters
+{
+	class QuantityToPacksConverter: IValueConverter
+	{		
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+
+			if (value is Product)
+			{
+				Product prod = (Product)value;
+				int packs = prod.WarehouseFreeQty / prod.PackSize;
+				return packs;
+			}
+			
+			else return null;				
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return DependencyProperty.UnsetValue;
+		}
+	}
+}
