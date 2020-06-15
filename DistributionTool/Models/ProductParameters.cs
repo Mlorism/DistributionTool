b9ada63363ebcb12ea2284de6e91110b798fff64
Marcin Kupiński <Mlorism@gmail.com>
2020-06-15 23:15:43 +0200

    ProductParameters model created.

---
+++ DistributionTool/Models/ProductParameters.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistributionTool.Models
+{
+	/// <summary>
+	/// Class model representing product parameters.
+	/// </summary>
+	class ProductParameters
+	{
+		/// <summary>
+		/// Price look-up code.
+		/// </summary>
+		public int PLU { get; set; }
+		/// <summary>
+		/// Store grade.
+		/// </summary>
+		public char Grade { get; set; }
+		/// <summary>
+		/// Minimum quantity for the grade.
+		/// </summary>
+		public int Min { get; set; }
+		/// <summary>
+		/// Maximum quantity for the grade.
+		/// </summary>
+		public int Max { get; set; }
+		/// <summary>
+		/// Minimum cover for the grade.
+		/// </summary>
+		public int Cover { get; set; }
+	}
+}
