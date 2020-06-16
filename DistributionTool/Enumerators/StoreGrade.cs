4ba0d6c7dafb8947258399c99234670b4329753b
Marcin Kupiński <Mlorism@gmail.com>
2020-06-16 16:53:30 +0200

    StoreGrade enum created.

---
+++ DistributionTool/Enumerators/StoreGrade.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistributionTool.Enumerators
+{
+	/// <summary>
+	/// Each store is assigned to a grade. 
+	/// </summary>
+	public enum StoreGrade
+	{
+		/// <summary>
+		/// Best stores.
+		/// </summary>
+		A,
+		/// <summary>
+		/// Average stores.
+		/// </summary>
+		B,
+		/// <summary>
+		/// Worst stores.
+		/// </summary>
+		C
+	}
+}
