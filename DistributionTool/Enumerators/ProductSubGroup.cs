162ebcc1340a1d8ec051d3d878aa86f8007a71e6
Marcin Kupiński <Mlorism@gmail.com>
2020-06-17 19:41:16 +0200

    Product Group and SubGroup enums created.

---
+++ DistributionTool/Enumerators/ProductSubGroup.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistributionTool.Enumerators
+{
+	public enum ProductSubGroup
+	{
+		
+		/// <summary>
+		/// RoomDecorations
+		/// </summary>
+		Candles,
+		Frames,
+		Vases,
+		Pillows,
+		Blankets,
+		/// <summary>
+		/// KitchenDining
+		/// </summary>
+		PlatesBowls,		
+		Cutlery,
+		KitchenTools,
+		PotsPans,
+		Kettles,
+		Glasses,
+		MugsCups,
+		/// <summary>
+		/// Bathroom
+		/// </summary>
+		BathroomAccessories,
+		Sponges,
+		Towels,
+		/// <summary>
+		/// CleaningStorage
+		/// </summary>
+		Mops,
+		Brushes,
+		Rags,
+		CardboardBoxes,
+		PlaticBoxes
+	}
+}
