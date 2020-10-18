bcadf8ee3d991429b9eaea9c3fed6beafc58c5b2
Marcin Kupiński <Mlorism@gmail.com>
2020-10-18 21:13:40 +0200

    Created structs and ObservableCollections for distribution summary.

--- DistributionTool/ViewModels/SummaryViewModel.cs
+++ DistributionTool/ViewModels/SummaryViewModel.cs
@@ -1,6 +1,9 @@
-﻿using DistributionTool.Interfaces;
+﻿using DistributionTool.Enumerators;
+using DistributionTool.Interfaces;
+using DistributionTool.Models;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,11 +12,53 @@ namespace DistributionTool.ViewModels
 {
 	class SummaryViewModel : BaseViewModel, ITab
 	{
+		#region Commands
+
+		#endregion
+
+		#region Properties and structures
+
+		struct productSummary
+		{
+			int productNo;
+			ProductGroupEnum group;
+			ProductSubGroupEnum subgroup;
+			int qty;
+			double retail;
+		}
+
+		struct groupSummary
+		{
+			ProductGroupEnum group;
+			double retail;
+		}
+
+		struct subGroupSummary
+		{
+			ProductSubGroupEnum subgroup;
+			double retail;
+		}
+
+		static ObservableCollection<productSummary> productSummaryList { get; set; }
+		static ObservableCollection<groupSummary> groupList { get; set; }
+		static ObservableCollection<subGroupSummary> subGroupList { get; set; }
+		static ObservableCollection<ProductDistribution> ProductDistributionList { get; set; }
+
+		#endregion
+
+
 		#region Constructor
 		public SummaryViewModel()
 		{
 			TabName = "Summary";
 		}
+		#endregion
+
+		#region Methods
+		public void CalculateSummary(object x)
+		{
+
+		} // CalculateSummary()
 		#endregion
 
 	}
